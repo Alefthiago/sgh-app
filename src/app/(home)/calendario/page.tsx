d2c81b3075e4437f91aa6ae2fbc16cc7bc8ba519
Alef Thiago da Silva <alefsilvadn@gmail.com>
2024-01-31 13:27:49 -0300

    commit

--- src/app/(home)/calendario/page.tsx
+++ src/app/(home)/calendario/page.tsx
@@ -4,7 +4,7 @@ import Calendario from "@/components/calendario/Calendario";
 
 export default function Home() {
     return (
-        <div className="flex flex-col items-center justify-center w-full h-4/5 sm:overflow-hidden overflow-scroll">
+        <div className="flex flex-col items-center justify-center w-full h-full sm:overflow-hidden overflow-scroll">
             <Calendario />
         </div>
     )
